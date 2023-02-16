4d474ec588f700b34e11397f4d799d15cdbd38d2
Timofey Silivonchyk <silivon4iktima@icloud.com>
2023-02-16 18:02:16 +0300

    part one complite

--- tasks/dict_tasks/get_set.py
+++ tasks/dict_tasks/get_set.py
@@ -10,8 +10,8 @@ from typing import Any
 
 
 def get_or_set(collection: dict, key: Any) -> Any:
-    # TODO вставить код сюда
-    result = None
+
+    result = collection.get(key, )
     return result
 
 
